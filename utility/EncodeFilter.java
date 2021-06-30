f7bc4804fb5471cdb15ec7316962b13f467c6d68
chw0000 <85987651+chw0000@users.noreply.github.com>
2021-06-30 16:24:20 +0900

    Add files via upload

---
+++ utility/EncodeFilter.java
@@ -0,0 +1,36 @@
+package net.utility;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodeFilter implements Filter{
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+		
+	}//doFilter() end
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}//class end
